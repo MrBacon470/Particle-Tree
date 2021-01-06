ed0495f1be348a4d40646a7918f69599364945b7
MrBacon470 <72811949+MrBacon470@users.noreply.github.com>
2021-01-06 12:38:58 -0600

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,4 +1,4 @@
-addLayer("p", {
+addLayer("g", {
     name: "Generators", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "G", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
@@ -6,7 +6,7 @@ addLayer("p", {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#0000ff",
+    color: "#6458E8",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "Protons", // Name of prestige currency
     baseResource: "Atoms", // Name of resource prestige is based on
@@ -23,6 +23,34 @@ addLayer("p", {
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "g", description: "G: Reset for Protons", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true}
+})
+addLayer("i", {
+    name: "Ionizer", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "I", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#E87B6F",
+    requires: new Decimal(100), // Can be a function that takes requirement increases into account
+    resource: "Electrons", // Name of prestige currency
+    baseResource: "Atoms", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "i", description: "G: Reset for Protons", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
 })
