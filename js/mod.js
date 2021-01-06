eb445b0e1c3160ed3ca319558ea06fa7c16a6c3b
MrBacon470 <72811949+MrBacon470@users.noreply.github.com>
2021-01-06 16:33:53 -0600

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -12,14 +12,14 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "4 Layers",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+		- Added 4 Layers.<br>
+		- Added 4 Upgrades to First Layer.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
